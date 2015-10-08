bcbb49ead85b12f3761abb61a5819012dee92ab2
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-08 16:02:25 -0500

    Strip hard-coded api token from main.js.

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var Slack = require('slack-client');
+// FIXME: Find a portable way to add our project files to NODE_PATH
+var creds = require('./credentials/testbot.js');
 
-var slackToken = 'xoxb-12146415603-Ed63RLooAVPojuati2xI959e' // Add a bot at https://my.slack.com/services/new/bot and copy the token here.
-var autoReconnect = true // Automatically reconnect after an error response from Slack.
-var autoMark = true // Automatically mark each message as read after it is processed.
+var slackToken = creds.api_token;
+var autoReconnect = true; // Automatically reconnect after an error response from Slack.
+var autoMark = true; // Automatically mark each message as read after it is processed.
 
 var slack = new Slack(slackToken, autoReconnect, autoMark)
 
 slack.on('open', function(){
-    console.log("Connected to"+slack.team.name + slack.self.name);
+    console.log("Connected to %s %s", slack.team.name, slack.self.name);
 });
 
 slack.on('message', function(message){
-    console.log(message);
+    console.log("%s: %s", new Date().toISOString(), message);
 });
 
 slack.on('error', function(err){
-    console.error("Error", err);
+    console.error("%s: error: %s", new Date().toISOString(), err);
 });
 
 slack.login();
