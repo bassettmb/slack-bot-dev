eccd3c061e5044164c3c41da23cb54f780bdef4e
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-18 19:26:09 -0500

    Add adaptor for wrapping handlers which provide both match and execute.

---
+++ bin/handler_adaptor.js
@@ -0,0 +1,16 @@
+module.exports = (function() {
+  var def = require('./def');
+  var Handler = require('./handler');
+  HandlerAdaptor = def.type(Handler, function(handler) {
+    this.def_prop('handler', handler);
+  });
+  HandlerAdaptor.def_method(function on_message(msg) {
+    if (!this.handler.match(msg))
+      return msg;
+
+    this.handler.execute(msg);
+    return null;
+  });
+  return HandlerAdaptor;
+})();
+
