94b1aa5d7842e73ab3f01d6e13db541cb566c6cb
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-19 09:58:33 -0500

    Fix accidental creation of global variable.

--- bin/handler_adaptor.js
+++ bin/handler_adaptor.js
@@ -1,7 +1,7 @@
 module.exports = (function() {
   var def = require('./def');
   var Handler = require('./handler');
-  HandlerAdaptor = def.type(Handler, function(handler) {
+  var HandlerAdaptor = def.type(Handler, function(handler) {
     this.def_prop('handler', handler);
   });
   HandlerAdaptor.def_method(function on_message(msg) {
