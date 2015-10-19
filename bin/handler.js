275a3e9aacfaf71bee03bae8e4baf1f38bb44809
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-19 09:58:01 -0500

    Refactor for clarity.

--- bin/handler.js
+++ bin/handler.js
@@ -1,4 +1,6 @@
 module.exports = (function() {
   var def = require('./def');
-  return def.type().def_method(function on_message(ev) { return ev; });
+  var Handler = def.type();
+  Handler.def_method(function on_message(ev) { return ev; });
+  return Handler;
 })(); 
