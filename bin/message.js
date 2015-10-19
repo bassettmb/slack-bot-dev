a9050bb0029538eb3af00f6d182eb25454ba3859
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-19 08:45:07 -0500

    Refactor to use method calls rather than direct def calls.

--- bin/message.js
+++ bin/message.js
@@ -7,25 +7,24 @@ module.exports = (function() {
 
   var Message = def.type(function(raw_msg) {
     guard_undef(raw_msg);
-    var def_prop = def.prop.bind(this);
-    def_prop('raw', raw_msg);
-    def_prop('channel', slack.getChannelGroupOrDMByID(raw_msg.channel));
-    def_prop('user', slack.getUserByID(raw_msg.user));
-    def_prop('text', raw_msg.text);
+    this.def_prop('raw', raw_msg);
+    this.def_prop('channel', slack.getChannelGroupOrDMByID(raw_msg.channel));
+    this.def_prop('user', slack.getUserByID(raw_msg.user));
+    this.def_prop('text', raw_msg.text);
   });
 
-  def.method(Message, function send_to(name, text) {
+  Message.def_method(function send_to(name, text) {
     return this.respond('@' + name + ': ' + text);
   });
 
-  def.method(Message, function reply(text) {
+  Message.def_method(function reply(text) {
     return this.send_to(this.raw_msg.user.name, text);
   });
 
-  def.method(type, function respond(text) {
+  Message.def_method(function respond(text) {
     this.channel.send(text);
   });
 
-  return type;
+  return Message;
 
 })();
