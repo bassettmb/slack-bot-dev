1ca475f286cafe88e7270f7dfd7cb2c4e35d8be4
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-19 10:00:06 -0500

    Update jokes handler to conform to new interface.

--- bin/jokes.js
+++ bin/jokes.js
@@ -3,13 +3,15 @@ module.exports = (function() {
   var def = require('./def');
   var Handler = require('./handler');
 
-  var Jokes = def.type();
+  var Jokes = def.type(Handler, function() {
+    Handler.call(this);
+  });
 
   Jokes.def_method(function match(msg) {
     return /[tT]ell a joke/.test(msg);
   });
 
-  Jokes.def_method(function execute(msg) {
+  Jokes.def_method(function on_message(msg) {
     if (this.match(msg.text))
       msg.reply('No.');
   });
