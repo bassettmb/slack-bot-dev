50518dd634ddb87d22750ed9914202fd1151e206
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-21 11:53:40 -0500

    Switch to 4-space indents to match existing file style.

--- src/calculator.js
+++ src/calculator.js
@@ -1,20 +1,20 @@
 define(function() {
-  var match = function(message)
-  {
-      return (message.text.toLowerCase().indexOf("calculate") == 0 );
-  }
+    var match = function(message)
+    {
+        return (message.text.toLowerCase().indexOf("calculate") == 0 );
+    }
 
-  var calculate = function(message)
-  {
-      text = message.text;
-      return text;
-  }
+    var calculate = function(message)
+    {
+        text = message.text;
+        return text;
+    }
 
-  var exports =
-  {
-      match: match,
-      execute: calculate
-  };
+    var exports =
+    {
+        match: match,
+        execute: calculate
+    };
 
-  return exports;
+    return exports;
 });
