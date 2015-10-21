216e2ded7d54451db200cb2031d0134262f810d8
Michael Bassett <michael.b.bassett@gmail.com>
2015-10-21 12:13:02 -0500

    Update imports.

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,9 @@
 define(['lib/def', 'lib/util'], function(Def, Util) {
 
-  var undef = Util.undef;
-  var id = Util.id;
-
   var Handler = Def.type();
 
   Handler.def_method(function on_message(ev, cont) {
-    return undef(cont) ? id(ev) : cont(ev);
+    return Util.undef(cont) ? Util.id(ev) : cont(ev);
   });
 
   return Handler;
