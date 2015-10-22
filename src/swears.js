9ddedabe2de327d67d8c752b1837fad4724dfbe1
Connor <connor.r.mcgowan@vanderbilt.edu>
2015-10-22 00:02:53 -0500

    First attempt at unit tests for swears handler

--- src/swears.js
+++ src/swears.js
@@ -4,7 +4,9 @@ define(['lib/def', 'lib/util', 'handler', 'fs'], function(Def, Util, Handler, FS
     var REPLY_POSTFIX = "' instead!";
 
     var Swears = Def.type(Handler, function(dict) {
-        if (!dict)
+        if (typeof dict !== 'object') {
+            throw new TypeError('Swear dictionary must be an object');
+        } else if (!dict)
             dict = {};
         this.constructor.__super__.call(this);
         this.def_prop('dict', dict);
@@ -26,20 +28,24 @@ define(['lib/def', 'lib/util', 'handler', 'fs'], function(Def, Util, Handler, FS
 
         if (matched) {
             censoredText = REPLY_PREFIX + censoredText + REPLY_POSTFIX;
-            msg.reply(censoredText, cont);
+            return msg.reply(censoredText, cont);
         } else {
-            cont(msg)
+            return cont(msg)
         }
     });
 
     Swears.def_static_method(function create_dict(filepath, cont) {
         return FS.readFile(filepath, function(err, contents) {
-            var swearsArray = Util.lines(contents.toString().toLowerCase());
-            var swearsMap = {};
-            for (var i = 0; i < swearsArray.length; i++) {
-                swearsMap[swearsArray[i]] = true;
+            if (err) {
+                return cont(err, contents);
+            } else {
+                var swearsArray = Util.lines(contents.toString().toLowerCase());
+                var swearsMap = {};
+                for (var i = 0; i < swearsArray.length; i++) {
+                    swearsMap[swearsArray[i]] = true;
+                }
+                return cont(err, swearsMap);
             }
-            return cont(err, swearsMap);
         });
     });
 
